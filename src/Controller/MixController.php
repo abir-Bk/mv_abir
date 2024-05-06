39d39e2f3c1f6137a91270bc255f5e7233cfaa55
abir-Bk <benkhlifaabiir@gmail.com>
2024-05-06 09:43:52 +0100

    "The Request Object"

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class MixController extends AbstractController{
+
+class MixController extends AbstractController
+{
     #[Route('/mix/new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
@@ -18,19 +22,34 @@ class MixController extends AbstractController{
         $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
+
         $entityManager->persist($mix);
         $entityManager->flush();
+
         return new Response(sprintf(
             'Mix %d is %d tracks of pure 80\'s heaven',
             $mix->getId(),
             $mix->getTrackCount()
         ));
     }
+
     #[Route('/mix/{id}', name: 'app_mix_show')]
     public function show(VinylMix $mix): Response
     {
         return $this->render('mix/show.html.twig', [
-           'mix' => $mix,
+            'mix' => $mix,
         ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        dd($mix);
     }
 }
